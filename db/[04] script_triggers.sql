489988f13c04044e500b1724f4d39e0e7aa9be02
Hiago Adao <hiago@registro.rs>
2021-10-07 16:47:26 -0300

    feat: Adicionando exemplos para todas as triggers.
    Adicionando exemplos para questões da atividade 4:
    [X] a
    [X] b
    [X] c
    [X] d
    [X] e
    [X] f

--- db/[04] script_triggers.sql
+++ db/[04] script_triggers.sql
@@ -23,24 +23,31 @@ CREATE TRIGGER trg_reserva_ingressos_se_disponiveis
 	EXECUTE PROCEDURE trabalho_g1.fn_reserva_ingressos_se_disponiveis();
 
 -- Exemplos para 4a
--- Erro
+-- SELECT * FROM trabalho_g1.sessao WHERE ingressos_disponiveis != 0 LIMIT 1;
+-- SELECT * FROM trabalho_g1.reserva ORDER BY cod_reserva DESC;
+-- UPDATE trabalho_g1.sessao SET ingressos_disponiveis = 0 WHERE cod_sessao in (SELECT cod_sessao FROM trabalho_g1.sessao WHERE ingressos_disponiveis > 0 LIMIT 1);
+
+-- Erro (4a)
 -- INSERT INTO trabalho_g1.reserva(cod_pedido, cod_espetaculo, cod_sessao, cadeira)
 -- VALUES (
--- 	(select cod_pedido from trabalho_g1.pedido LIMIT 1),
--- 	(select cod_espetaculo from trabalho_g1.espetaculo LIMIT 1),
--- 	(select cod_sessao from trabalho_g1.sessao where ingressos_disponiveis = 0 LIMIT 1),
--- 	'A - 101'
+-- 	(SELECT cod_pedido FROM trabalho_g1.pedido LIMIT 1),
+-- 	(SELECT cod_espetaculo FROM trabalho_g1.espetaculo LIMIT 1),
+-- 	(SELECT cod_sessao FROM trabalho_g1.sessao WHERE ingressos_disponiveis = 0 LIMIT 1),
+-- 	'B-6'
 -- );
 
--- Sucesso
+-- Sucesso (4a, 4c)
 -- INSERT INTO trabalho_g1.reserva(cod_pedido, cod_espetaculo, cod_sessao, cadeira)
 -- VALUES (
--- 	(select cod_pedido from trabalho_g1.pedido LIMIT 1),
--- 	(select cod_espetaculo from trabalho_g1.espetaculo LIMIT 1),
--- 	(select cod_sessao from trabalho_g1.sessao where ingressos_disponiveis != 0 LIMIT 1),
--- 	'A - 101'
+-- 	(SELECT cod_pedido FROM trabalho_g1.pedido LIMIT 1),
+-- 	(SELECT cod_espetaculo FROM trabalho_g1.espetaculo LIMIT 1),
+-- 	(SELECT cod_sessao FROM trabalho_g1.sessao WHERE ingressos_disponiveis != 0 LIMIT 1),
+-- 	'B-6'
 -- );
 -- SELECT * FROM trabalho_g1.reserva ORDER BY cod_reserva DESC;
+-- SELECT * FROM trabalho_g1.sessao WHERE cod_sessao = (
+-- 	SELECT cod_sessao FROM trabalho_g1.reserva ORDER BY cod_reserva DESC LIMIT 1
+-- );
 
 
 -- ======================================================
@@ -52,7 +59,8 @@ CREATE TABLE trabalho_g1.historico_reserva (
 	cod_pedido INT NOT NULL,
 	cod_espetaculo INT NOT NULL,
 	cod_sessao INT NOT NULL,
-	cadeira VARCHAR(10) NOT NULL
+	cadeira VARCHAR(10) NOT NULL,
+	data_exclusao_reserva DATE DEFAULT CURRENT_DATE
 );
 
 CREATE OR REPLACE FUNCTION trabalho_g1.fn_mantem_hist_reserva()
@@ -71,8 +79,17 @@ CREATE TRIGGER trg_mantem_hist_reserva
 	FOR EACH ROW
 	EXECUTE PROCEDURE trabalho_g1.fn_mantem_hist_reserva();
 
--- Exemplos para 4b
--- ...
+-- Exemplos para 4b e 4d
+-- SELECT * FROM trabalho_g1.historico_reserva;
+-- DELETE FROM trabalho_g1.reserva
+-- WHERE cod_reserva = (
+-- 	SELECT cod_reserva FROM trabalho_g1.reserva ORDER BY cod_reserva DESC LIMIT 1
+-- );
+-- SELECT * FROM trabalho_g1.reserva ORDER BY cod_reserva DESC;
+-- SELECT * FROM trabalho_g1.historico_reserva;
+-- SELECT * FROM trabalho_g1.sessao WHERE cod_sessao = (
+-- 	SELECT cod_sessao FROM trabalho_g1.historico_reserva ORDER BY cod_hist_reserva DESC LIMIT 1
+-- );
 
 
 -- ======================================================
@@ -105,9 +122,6 @@ CREATE TRIGGER trg_decrementa_ingressos_disponiveis
 	FOR EACH ROW
 	EXECUTE PROCEDURE trabalho_g1.fn_decrementa_ingressos_disponiveis();
 
--- Exemplos para 4c
--- ...
-
 
 -- ======================================================
 
@@ -139,9 +153,6 @@ CREATE TRIGGER trg_incrementa_ingressos_disponiveis
 	FOR EACH ROW
 	EXECUTE PROCEDURE trabalho_g1.fn_incrementa_ingressos_disponiveis();
 
--- Exemplos para 4d
--- ...
-
 
 -- ======================================================
 
@@ -168,7 +179,22 @@ CREATE TRIGGER trg_inativar_pedido
 	EXECUTE PROCEDURE trabalho_g1.fn_inativar_pedido();
 
 -- Exemplos para 4e
--- ...
+-- SELECT * 
+-- FROM trabalho_g1.pedido
+-- WHERE cod_pedido IN (SELECT cod_pedido FROM trabalho_g1.reserva)
+-- ORDER BY cod_pedido;
+
+-- SELECT * FROM trabalho_g1.reserva WHERE cod_pedido = (
+-- 	SELECT cod_pedido 
+-- 	FROM trabalho_g1.pedido
+-- 	WHERE cod_pedido IN (SELECT cod_pedido FROM trabalho_g1.reserva)
+-- 	ORDER BY cod_pedido
+-- 	LIMIT 1
+-- );
+
+-- DELETE FROM trabalho_g1.pedido WHERE cod_pedido = 33;
+-- SELECT * FROM trabalho_g1.pedido ORDER BY cod_pedido;
+-- SELECT * FROM trabalho_g1.reserva WHERE cod_pedido = 33;
 
 
 -- ======================================================
@@ -181,19 +207,23 @@ $BODY$
 DECLARE
 	pedidos_ativos INT;
 	msg_padrao VARCHAR;
-	msg_pedidos_ativos VARCHAR := '';
+	msg_pedidos_ativos VARCHAR;
+	msg_exception VARCHAR;
 BEGIN
-	msg_padrao := 'Nao e possivel excluir um cliente, somente modificar seu status';
+	msg_padrao := 'Nao e possivel excluir um cliente, somente modificar seu status.';
+	msg_pedidos_ativos := 'Nao modifique o status do cliente para inativo, pois o mesmo possui pedidos ativos.';
     
 	SELECT count(status) INTO pedidos_ativos
 	FROM trabalho_g1.pedido
 	WHERE cod_cliente = OLD.cod_cliente AND status = 'A';
 
 	IF (pedidos_ativos > 0) THEN
-		RAISE EXCEPTION 'Nao modifique o status do cliente para inativo, pois o mesmo possui pedidos ativos';
+		msg_exception := CONCAT(msg_padrao, ' ', msg_pedidos_ativos);
+	ELSE
+		msg_exception := msg_padrao;
     END IF;
 
-	RAISE EXCEPTION '%', CONCAT(msg_padrao, '. ', msg_pedidos_ativos);
+	RAISE EXCEPTION '%', msg_exception;
 END
 $BODY$ LANGUAGE plpgsql;
 
@@ -203,4 +233,17 @@ CREATE TRIGGER trg_impossibilita_exclusao_cliente
 	EXECUTE PROCEDURE trabalho_g1.fn_impossibilita_exclusao_cliente();
 
 -- Exemplos para 4f
--- ...
+-- SELECT * FROM trabalho_g1.cliente;
+-- SELECT * FROM trabalho_g1.cliente WHERE cod_cliente IN (
+-- 	SELECT cod_cliente FROM trabalho_g1.pedido WHERE status = 'A'
+-- );
+
+-- SELECT *
+-- FROM trabalho_g1.pedido
+-- WHERE cod_cliente = 94;
+
+-- UPDATE trabalho_g1.pedido SET status = 'I' WHERE cod_cliente = 95;
+-- UPDATE trabalho_g1.pedido SET status = 'A' WHERE cod_cliente = 94;
+
+-- DELETE FROM trabalho_g1.cliente WHERE cod_cliente = 95;
+-- DELETE FROM trabalho_g1.cliente WHERE cod_cliente = 94;
